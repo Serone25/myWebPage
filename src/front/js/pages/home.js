778a72c7be7a67a53e803dc3f2b38f50316f6934
Serone25 <fran19842010@live.com>
2023-03-13 18:07:32 +0000

    Agua

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,8 +1,11 @@
-import React, { useContext, useState, useRef } from "react";
+import * as THREE from 'three';
+import React, { useContext, useState, useRef, useMemo, Suspense } from "react";
 import { Context } from "../store/appContext";
 
-import { Canvas, useFrame } from "@react-three/fiber";
-import{ Environment, OrbitControls, Stars, Text3D} from "@react-three/drei"
+import { Canvas, useFrame, useThree,useLoader, extend } from "@react-three/fiber";
+import{ Environment, OrbitControls, Stars, Text3D,Sky } from "@react-three/drei";
+import { Water } from 'three-stdlib';
+
 
 
 import "../../styles/home.css";
@@ -11,19 +14,30 @@ import font from "../../../../public/Inter_Bold.json"
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
-	const Texto3D = () => {
-		const ref = useRef();
-	  
-		useFrame((state, delta) => {
-		  ref.current.rotation.x += 0.01;
-		  ref.current.rotation.y += 0.01;
-		});
-	  
-		return (
-		  <Text3D font={font} ref={ref}>
-			Hello World!
-		  </Text3D>
-		);
+	extend({Water})
+
+	function Ocean() {
+		const ref = useRef()
+		const gl = useThree((state) => state.gl)
+		const waterNormals = useLoader(THREE.TextureLoader, '/waternormals.jpeg')
+		waterNormals.wrapS = waterNormals.wrapT = THREE.RepeatWrapping
+		const geom = useMemo(() => new THREE.PlaneGeometry(10000, 10000), [])
+		const config = useMemo(
+		  () => ({
+			textureWidth: 512,
+			textureHeight: 512,
+			waterNormals,
+			sunDirection: new THREE.Vector3(),
+			sunColor: 0xffffff,
+			waterColor: 0x001e0f,
+			distortionScale: 3.7,
+			fog: false,
+			format: gl.encoding
+		  }),
+		  [waterNormals]
+		)
+		useFrame((state, delta) => (ref.current.material.uniforms.time.value += delta))
+		return <water ref={ref} args={[geom, config]} rotation-x={-Math.PI / 2} />
 	  }
 	
 
@@ -42,12 +56,16 @@ export const Home = () => {
 					<Text3D font={font} size= "0.5"
 							height= "0.2"
 							curveSegments= "64"
-							position ={[1,0,0]}
+							position ={[1,1,0]}
 							>
 						HI! I'M  FRAN
 						<meshNormalMaterial/>
 					</Text3D>
-					<Texto3D/>
+					<Suspense fallback= {null}>
+					<Ocean/>
+					</Suspense>
+					
+					<Sky scale={1000} sunPosition={[0, 20, -1500]} turbidity={0} rayleigh={1} mieCoefficient={0.03} mieDirectionalG={0.04} />
 					<mesh>
 						<boxGeometry />
 						<meshStandardMaterial color="royalblue" roughness={0} metalness={0} />
