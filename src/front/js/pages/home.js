cd0f8ad8b9347c246c1ab1471e14253b4efc441e
Serone25 <fran19842010@live.com>
2023-03-13 16:33:10 +0000

    First Commit

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,26 +1,58 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useRef } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
+
+import { Canvas, useFrame } from "@react-three/fiber";
+import{ Environment, OrbitControls, Stars, Text3D} from "@react-three/drei"
+
+
 import "../../styles/home.css";
+import font from "../../../../public/Inter_Bold.json"
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
+	const Texto3D = () => {
+		const ref = useRef();
+	  
+		useFrame((state, delta) => {
+		  ref.current.rotation.x += 0.01;
+		  ref.current.rotation.y += 0.01;
+		});
+	  
+		return (
+		  <Text3D font={font} ref={ref}>
+			Hello World!
+		  </Text3D>
+		);
+	  }
+	
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
+		<div className="home_body">
+			<Canvas
+				className="canvas"
+				shadows
+				camera={{position:[0,30,60], fov : 15}}>
+				<Stars radius={50} depth={50} count={5000} saturation={0} />
+					<Environment  preset="night" />
+					<ambientLight/>
+					<pointLight position={[0,15,0]} color="#570c0c" castShadow intensity={5}/>{/*Esta encima del cubo*/}
+					<directionalLight position={[0,-15,0]} color="#570c0c" castShadow intensity={5}/>{/*Esta debajo del cubo*/}
+					<OrbitControls/>
+					<Text3D font={font} size= "0.5"
+							height= "0.2"
+							curveSegments= "64"
+							position ={[1,0,0]}
+							>
+						HI! I'M  FRAN
+						<meshNormalMaterial/>
+					</Text3D>
+					<Texto3D/>
+					<mesh>
+						<boxGeometry />
+						<meshStandardMaterial color="royalblue" roughness={0} metalness={0} />
+					</mesh>
+			</Canvas>
 		</div>
 	);
 };
